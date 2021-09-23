8c8d10eb510583b3ceb56588cf37524eb432fcce
atonyma7 <atonyma7@gmail.com>
2021-09-22 21:58:27 -0600

    Using mimetypes library

--- server.py
+++ server.py
@@ -31,17 +31,19 @@ import os
 # https://stackoverflow.com/a/32628115
 # https://www.codementor.io/@joaojonesventura/building-a-basic-http-server-from-scratch-in-python-1cedkg0842
 # https://uofa-cmput404.github.io/cmput404-slides/04-HTTP.html#/
+# https://stackoverflow.com/a/66012377
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
     #helper functions
     def send_content(self, file_path):
-        mime_type = 'text/html'
-        if file_path.endswith('.css'):
-            mime_type = 'text/css'
-        elif file_path.endswith('.html'):
+        mt = mimetypes.guess_type(file_path)
+        if mt:
+            print("Mime Type:", mt[0])
+        else:
+            #default to html
             mime_type = 'text/html'
-        f = open(file_path, 'r')
+        f = open(file_path, 'r+b')
         self.request.sendall(bytearray("HTTP/1.1 200 OK\r\n",'utf-8'))
         self.request.sendall(bytearray("Content-Type: {}\r\n".format(mime_type), 'utf-8'))
         self.request.sendall(bytearray("\r\n", 'utf-8'))
